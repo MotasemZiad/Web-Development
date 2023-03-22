fed49d5f8a91dbc9c45253900daf849f750fc16c
MotasemZiad <motasemabunima@gmail.com>
2023-03-22 18:24:48 +0200

    data types in js

--- 03_JS/MyFirstJSProject/app.js
+++ 03_JS/MyFirstJSProject/app.js
@@ -5,7 +5,35 @@ console.info("Info: hello world")
 console.error("Error: hello world")
 console.table(["Ahmed", "Mohammed", "Mahmoud"])
 console.log("Hello form the %cJS %cFile", "color:red; font-size:1.6em", "color:blue; font-size: 1.2em; font-weight: bold")
+/*  Data Types:
+        - string
+        - number (integer, floating-point)
+        - boolean
+        - object (array, map, null, object)
+        - undefined
+*/
+
 
 let fullName = "Motasem Abunima"
 let age = 23
-console.log(`hello, my name is ${fullName}. I am ${age} years old.`)
+let gpa = 85.51
+let isGraduated = true
+let skills = ["Linux", "Cloud Computing", "Programming"]
+let student = {
+    fullName: fullName,
+    age: age,
+    gpa: gpa,
+    isGraduated: isGraduated,
+    skills: skills
+}
+
+console.log(`fullName ${fullName}`, "Type Of: "+ typeof(fullName))
+console.log(`age ${age}`, "Type Of: "+ typeof(age))
+console.log(`gpa ${gpa}`, "Type Of: "+ typeof(gpa))
+console.log(`isGraduated ${isGraduated}`, "Type Of: "+ typeof(isGraduated))
+console.log(`skills ${skills}`, "Type Of: "+ typeof(skills))
+console.log(`skills ${skills[0]}`, "Type Of: "+ typeof(skills[0]))
+console.log(`student ${student}`, "Type Of: "+ typeof(student)) 
+console.log(`student ${student.fullName}`, "Type Of: "+ typeof(student.fullName)) 
+console.log(`null ${null}`, "Type Of: "+ typeof(null)) 
+console.log(`undefined ${undefined}`, "Type Of: "+ typeof(undefined)) 
