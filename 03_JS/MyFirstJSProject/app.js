9db63fa928cbd9e4f3e849d3d5d267e2f41f95d1
MotasemZiad <motasemabunima@gmail.com>
2023-03-22 17:45:58 +0200

    working with console

---
+++ 03_JS/MyFirstJSProject/app.js
@@ -0,0 +1,9 @@
+console.log("Log: hello world")
+console.warn("Warn: Hello World")
+console.debug("Debug: hello world")
+console.info("Info: hello world")
+console.error("Error: hello world")
+console.table(["Ahmed", "Mohammed", "Mahmoud"])
+
+
+console.log("Hello form the %cJS %cFile", "color:red; font-size:1.6em", "color:blue; font-size: 1.2em; font-weight: bold")
