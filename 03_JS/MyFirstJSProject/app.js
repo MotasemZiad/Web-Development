2cd86336be73843c9fb857a8239ea33f00650063
MotasemZiad <motasemabunima@gmail.com>
2023-03-27 16:17:29 +0200

    finish the if/else and switch challenges

--- 03_JS/MyFirstJSProject/app.js
+++ 03_JS/MyFirstJSProject/app.js
@@ -332,3 +332,43 @@ let price = null;
 // if (st.substring(0, 6).repeat(2) === "ElzeroElzero") {
 //   console.log("Good");
 // }
+
+let job = "Manager";
+let salary = 0;
+
+switch (job) {
+  case "Manager":
+    salary = 8000;
+    break;
+  case "IT":
+  case "Support":
+    salary = 6000;
+    break;
+  case "Developer":
+  case "Designer":
+    salary = 7000;
+    break;
+  default:
+    salary = 4000;
+}
+
+console.log(`Job: ${job}\tSalary: ${salary}`);
+
+let holidays = 0;
+let money = 0;
+
+if (holidays == 0) {
+  money = 5000;
+} else if (holidays == 1 || holidays == 2) {
+  money = 3000;
+} else if (holidays == 3) {
+  money = 2000;
+} else if (holidays == 4) {
+  money = 1000;
+} else if (holidays == 5) {
+  money = 0;
+} else {
+  money = 0;
+}
+
+console.log(`My money is ${money}`);
