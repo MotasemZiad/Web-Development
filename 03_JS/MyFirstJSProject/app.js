3a05a4044c010413ec57f1634b3d211a46dcf2f5
MotasemZiad <motasemabunima@gmail.com>
2023-04-11 03:51:58 +0200

    good result

--- 03_JS/MyFirstJSProject/app.js
+++ 03_JS/MyFirstJSProject/app.js
@@ -1579,9 +1579,4 @@ let stopButton = document.querySelector("button");
 let n1 = [10, 30, 10, 20];
 let n2 = [30, 20, 10];
 
-console.log(
-  `Your final result is`,
-  Math.min(...n1) * Math.min(...n2) +
-    Math.min(...n1) * Math.min(...n2) +
-    Math.min(...n1)
-); // 210
+console.log([...n1, ...n2].length * new Set(n1).size * Math.min(...n2));
